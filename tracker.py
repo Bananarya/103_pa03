063c6a6868430e2eb776245533db3ce84fa031e0
yueliulu <73615797+yueliulu@users.noreply.github.com>
2023-03-25 18:46:26 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -2,47 +2,20 @@ from transaction import transaction
 import sys
 
 """Creat method to handle different activity"""
-# def show_transaction():
-
-#     if __name__=="__main__":
-#         transact=transaction()
-#         print("""
-#             Welcome to the transaction sql interactive platform, please type the number to make a request:  
-#             0. quit
-#             4. show transactions
-#             5. add transaction
-#             6. delete transaction
-#             7. summarize transactions by date
-#             8. summarize transactions by month
-#             9. summarize transactions by year
-#             10. summarize transactions by category
-#             11. print this menu
-#             """)
-#         action_num=int(input())
-#         if(action_num==0):
-#             print("Goodbye!")
-#             return
-#         elif(action_num==5):
-#             print("Pleas tell me the information of the transaction you want to add")
-#             item=input()
-#         elif(action_num==6):
-#             print("Pleas tell me the item #(rowid) of the transaction you want to delete")
-#             rowid=input()
-
 
 def print_usage():
     ''' print an explanation of how to use this command '''
     print("""
-            Welcome to the transaction sql interactive platform, please type the number to make a request:  
-            0. quit
-            4. show transactions (show)
-            5. add transaction (add)
-            6. delete transaction (delete)
-            7. summarize transactions by date
-            8. summarize transactions by month
-            9. summarize transactions by year
-            10. summarize transactions by category
-            11. print this menu
+            Welcome to the transaction sql interactive platform, please type the command to make a request:  
+            q: quit
+            show: show transactions
+            add: add transaction
+            delete: delete transaction 
+            std: summarize transactions by date
+            stm: summarize transactions by month
+            sty: summarize transactions by year
+            stc: summarize transactions by category
+            p: print this menu
             """
             )
 
@@ -56,6 +29,7 @@ def print_todos(todos):
     print('-'*40)
     for item in todos:
         values = tuple(item.values()) #(rowid,title,desc,completed)
+        values = values[:len(values)-2]
         print("%-10s %-10s %-10s %-10s %-50s"%values)
 
 def process_args(arglist):
@@ -65,8 +39,6 @@ def process_args(arglist):
         print_usage()
     elif arglist[0]=="show":
         print_todos(todos = todolist.selectAll())
-    elif arglist[0]=="showcomplete":
-        print_todos(todolist.selectCompleted())
     elif arglist[0]=='add':
         print(arglist)
         if len(arglist)!=5:
@@ -74,16 +46,31 @@ def process_args(arglist):
         else:
             todo = {'amount':arglist[1],'category':arglist[2],'date':arglist[3], 'description': arglist[4]}
             todolist.add(todo)
-    elif arglist[0]=='complete':
-        if len(arglist)!= 2:
-            print_usage()
-        else:
-            todolist.setComplete(arglist[1])
     elif arglist[0]=='delete':
         if len(arglist)!= 2:
             print_usage()
         else:
             todolist.delete(arglist[1])
+    elif arglist[0]=='std':
+        if len(arglist)!=2:
+            print_usage()
+        else:
+            print_todos(todos = todolist.selectDate(arglist[1]))
+    elif arglist[0]=='stm':
+        if len(arglist)!=2:
+            print_usage()
+        else:
+            print_todos(todos = todolist.selectMonth(arglist[1]))
+    elif arglist[0]=='sty':
+        if len(arglist)!=2:
+            print_usage()
+        else:
+            print_todos(todos = todolist.selectYear(arglist[1]))
+    elif arglist[0]=='stc':
+        if len(arglist)!=2:
+            print_usage()
+        else:
+            print_todos(todos = todolist.selectCategory(arglist[1]))
     else:
         print(arglist,"is not implemented")
         print_usage()

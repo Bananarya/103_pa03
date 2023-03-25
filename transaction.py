ae4e5e3c1d22568f4fd60595ffb9b4e553c7dc3b
yueliulu <73615797+yueliulu@users.noreply.github.com>
2023-03-25 19:23:08 -0400

    Update transaction.py
    Added 0 and 11.

--- transaction.py
+++ transaction.py
@@ -1,74 +1,134 @@
-'''
-transaction.py is an Object Relational Mapping to the transaction database
+from transaction import transaction
+import sys
 
-The ORM will work map SQL rows with the schema
-    (item#,amount,category,date,description)
-to Python Dictionaries as follows:
+"""Creat method to handle different activity"""
+# def show_transaction():
 
-(5,5,'food',2023/3/24,'something to eat') <-->
-{rowid:5,title:5,category:'food',date:2023/3/24,description:'something to eat')
+#     if __name__=="__main__":
+#         transact=transaction()
+#         print("""
+#             Welcome to the transaction sql interactive platform, please type the number to make a request:  
+#             0. quit
+#             4. show transactions
+#             5. add transaction
+#             6. delete transaction
+#             7. summarize transactions by date
+#             8. summarize transactions by month
+#             9. summarize transactions by year
+#             10. summarize transactions by category
+#             11. print this menu
+#             """)
+#         action_num=int(input())
+#         if(action_num==0):
+#             print("Goodbye!")
+#             return
+#         elif(action_num==5):
+#             print("Pleas tell me the information of the transaction you want to add")
+#             item=input()
+#         elif(action_num==6):
+#             print("Pleas tell me the item #(rowid) of the transaction you want to delete")
+#             rowid=input()
 
-In place of SQL queries, we will have method calls.
 
-This app will store the data in a SQLite database ~/transaction.db
+def print_usage():
+    ''' print an explanation of how to use this command '''
+    print("""
+            Welcome to the transaction sql interactive platform, please type the command to make a request:  
+            q: quit
+            show: show transactions
+            add: add transaction
+            delete: delete transaction 
+            std: summarize transactions by date
+            stm: summarize transactions by month
+            sty: summarize transactions by year
+            stc: summarize transactions by category
+            p: print this menu
+            """
+            )
 
-'''
-import sqlite3
-import os
+def print_todos(todos):
+    ''' print the todo items '''
+    if len(todos)==0:
+        print('no tasks to print')
+        return
+    print('\n')
+    print("%-10s %-10s %-10s %-10s %-50s"%('item #','amount','category','date','description'))
+    print('-'*40)
+    for item in todos:
+        values = tuple(item.values()) #(rowid,title,desc,completed)
+        values = values[:len(values)-2]
+        print("%-10s %-10s %-10s %-10s %-50s"%values)
 
-def toDict(t):
-    ''' t is a tuple (item#,amount, category,date,description), according to the demo, item# is the rowid'''
-    print('t='+str(t))
-    todo = {'rowid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3],'description':t[4],'year':t[5],'month':t[6]}
-    return todo
+def process_args(arglist):
+    ''' examine args and make appropriate calls to TodoList'''
+    todolist = transaction()
+    if arglist==[]:
+        print_usage()
+    elif arglist[0]=="show":
+        print_todos(todos = todolist.selectAll())
+    elif arglist[0]=='add':
+        if len(arglist)<5:
+            print("Illegal argument")
+        else:
+            arglist = ['add',arglist[1],arglist[2],arglist[3], " ".join(arglist[4:])]
+            todo = {'amount':arglist[1],'category':arglist[2],'date':arglist[3], 'description': arglist[4]}
+            todolist.add(todo)
+            print("Transaction added")
+    elif arglist[0]=='delete':
+        if len(arglist)!= 2:
+            print("Illegal argument")
+        else:
+            todolist.delete(arglist[1])
+            print("Transaction deleted")
+    elif arglist[0]=='std':
+        if len(arglist)!=2:
+            print("Illegal argument")
+        else:
+            print_todos(todos = todolist.selectDate(arglist[1]))
+    elif arglist[0]=='stm':
+        if len(arglist)!=2:
+            print("Illegal argument")
+        else:
+            print_todos(todos = todolist.selectMonth(arglist[1]))
+    elif arglist[0]=='sty':
+        if len(arglist)!=2:
+            print("Illegal argument")
+        else:
+            print_todos(todos = todolist.selectYear(arglist[1]))
+    elif arglist[0]=='stc':
+        if len(arglist)!=2:
+            print("Illegal argument")
+        else:
+            print_todos(todos = todolist.selectCategory(arglist[1]))
+    elif arglist[0]=='p':
+        print_usage()
+    else:
+        print(arglist,"is not implemented")
+        print_usage()
 
-class transaction():
-    def __init__(self):
-        self.runQuery('''CREATE TABLE IF NOT EXISTS transaction_table
-                    (amount int, category text, date text, description text, year text, month text)''',())
+
+def toplevel():
+    ''' read the command args and process them'''
+    if len(sys.argv)==1:
+        # they didn't pass any arguments, 
+        # so prompt for them in a loop
+        print_usage()
+        args = [] 
+        while args!=['']:
+            args = input("command> ").split(' ')
+            if args[0]=='q':
+                break
+            else:
+                process_args(args)
+                print('-'*40+'\n'*3)
+                print_usage()
+    else:
+        # read the args and process them
+        args = sys.argv[1:]
+        process_args(args)
+        print('-'*40+'\n'*3)
+
     
-    def selectDate(self,date):  
-        ''' return all of the uncompleted tasks as a list of dicts.'''
-        return self.runQuery("SELECT rowid,* from transaction_table where date=(?)",(date,))
 
-    def selectCategory(self,category):
-        return self.runQuery("SELECT rowid,* from transaction_table where category=(?)",(category,))
+toplevel()
 
-    def selectMonth(self,month):
-        return self.runQuery("SELECT rowid,* from transaction_table where month=(?)",(month,))
-
-    def selectYear(self,year):
-        return self.runQuery("SELECT rowid,* from transaction_table where year=(?)",(year,))
-
-    def selectAll(self):
-        ''' return all of the tasks as a list of dicts.'''
-        return self.runQuery("SELECT rowid,* from transaction_table",())
-
-    def add(self,item):
-        ''' create a todo item and add it to the todo table '''
-        date = item['date'].split('/')
-        year = date[0]
-        month = date[1]
-        return self.runQuery("INSERT INTO transaction_table VALUES(?,?,?,?,?,?)",(item['amount'],item['category'],item['date'],item['description'],year,month))
-
-    def delete(self,rowid):
-        ''' delete a todo item '''
-        return self.runQuery("DELETE FROM transaction_table WHERE rowid=(?)",(rowid,))
-    #not a useful method, but can be modified if u need
-    """
-    def setComplete(self,rowid):
-        ''' mark a todo item as completed '''
-        return self.runQuery("UPDATE transaction_table SET completed=1 WHERE rowid=(?)",(rowid,))
-    """
-    def runQuery(self,query,tuple):
-        ''' return all of the uncompleted tasks as a list of dicts.'''
-        con= sqlite3.connect(os.getenv('HOME')+'/transaction1.db')
-        cur = con.cursor() 
-        cur.execute(query,tuple)
-        tuples = cur.fetchall()
-        con.commit()
-        con.close()
-        return [toDict(t) for t in tuples]
-    
-if __name__=="__main__":
-    transact=transaction()
